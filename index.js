226edf7bb65a6c20c20a6ec0b2e727b00134bf23
Niko Fotopoulos <fottopoulos@gmail.com>
2023-09-05 20:02:01 +0200

    Print the hashtags in BnW

---
+++ index.js
@@ -0,0 +1,14 @@
+const bigLine = '#'.repeat(31);
+const smallLine = '#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5);
+
+console.log(bigLine);
+console.log(bigLine);
+console.log(bigLine);
+
+console.log(smallLine);
+console.log(smallLine);
+console.log(smallLine);
+
+console.log(bigLine);
+console.log(bigLine);
+console.log(bigLine);
