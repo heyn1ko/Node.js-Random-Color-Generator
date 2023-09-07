73bd5396b94bc17edc2de71ac08144e419e2e5cc
Niko Fotopoulos <fottopoulos@gmail.com>
2023-09-07 21:10:23 +0200

    Implement the features.

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
 #Node.js Random Color Generator
 When a user enters node index.js in the command line, a block of approximately
+31x9 # characters colored with a random color (using hex code, e.g. #ff0000) should be generated.
 
-[X] 31x9 # characters
-
-- colored with a random color (using hex code, e.g. #ff0000) should be generated.
-  ############################### # ############################### # ############################### # ##### ##### # ##### #edff28 ##### # ##### ##### # ############################### # ############################### # ###############################
-
-  it should also be able to accept the request for a:
+it should also be able to accept the request for a:
 
 - hue (e.g. green or red or blue)
 - luminosity (e.g. light or dark)
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const bigLine = '#'.repeat(31);
-const smallLine = '#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5);
+//PREVIOUS CODE
+// const bigLine = '#'.repeat(31);
+// const smallLine = '#'.repeat(5) + ' '.repeat(21) + '#'.repeat(5);
 
-console.log(bigLine);
-console.log(bigLine);
-console.log(bigLine);
+// console.log(bigLine);
+// console.log(bigLine);
+// console.log(bigLine);
 
-console.log(smallLine);
-console.log(smallLine);
-console.log(smallLine);
+// console.log(smallLine);
+// console.log(smallLine);
+// console.log(smallLine);
+// console.log(bigLine);
+// console.log(bigLine);
+// console.log(bigLine);
 
-console.log(bigLine);
-console.log(bigLine);
-console.log(bigLine);
+import { argv } from 'node:process';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
+
+const color = randomColor({ hue: argv[2], luminosity: argv[3] });
+console.log(
+  chalk.hex(color).bold(`###############################
+###############################
+###############################
+#####                     #####
+#####      ${color}        #####
+#####                     #####
+###############################
+###############################
+###############################`),
+);
